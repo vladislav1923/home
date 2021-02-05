2d5e886a7a3da33342645230bf68d2c8c36bd673
kozak <vladislav1923@bk.ru>
2021-02-05 10:13:10 +0300

    scale landing is done

--- src/components/shared/icons/Scope.tsx
+++ src/components/shared/icons/Scope.tsx
@@ -2,7 +2,7 @@
 import { Icon } from '../../../interfaces';
 
 export const Scope = ({ className }: Icon) => (
-    <svg className={className} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-        <path fillRule="evenodd" clipRule="evenodd" d="M3.6 21.6C2.9376 21.6 2.4 21.0612 2.4 20.4V16.8H0V20.4C0 22.3848 1.6152 24 3.6 24H7.2V21.6H3.6ZM21.6 20.4C21.6 21.0624 21.0612 21.6 20.4 21.6H16.8V24H20.4C22.3848 24 24 22.3848 24 20.4V16.8H21.6V20.4ZM20.4 2.4C21.0624 2.4 21.6 2.9388 21.6 3.6V7.2H24V3.6C24 1.6152 22.3848 0 20.4 0H16.8V2.4H20.4ZM2.4 7.2H0V3.6C0 1.6152 1.6152 0 3.6 0H7.2V2.4H3.6C2.9388 2.4 2.4 2.9376 2.4 3.6V7.2Z" fill="black"/>
-    </svg>
+  <svg className={className} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <path fillRule="evenodd" clipRule="evenodd" d="M3.6 21.6C2.9376 21.6 2.4 21.0612 2.4 20.4V16.8H0V20.4C0 22.3848 1.6152 24 3.6 24H7.2V21.6H3.6ZM21.6 20.4C21.6 21.0624 21.0612 21.6 20.4 21.6H16.8V24H20.4C22.3848 24 24 22.3848 24 20.4V16.8H21.6V20.4ZM20.4 2.4C21.0624 2.4 21.6 2.9388 21.6 3.6V7.2H24V3.6C24 1.6152 22.3848 0 20.4 0H16.8V2.4H20.4ZM2.4 7.2H0V3.6C0 1.6152 1.6152 0 3.6 0H7.2V2.4H3.6C2.9388 2.4 2.4 2.9376 2.4 3.6V7.2Z" fill="black" />
+  </svg>
 );
